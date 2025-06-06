a761bcc09144a7b37113dd94fcace76b061c2304
sandeep khariwal <sandeepkharival123@gmail.com>
2025-06-06 13:14:12 +0530

    formatting

--- src/controller/student.controller.ts
+++ src/controller/student.controller.ts
@@ -4,17 +4,18 @@ import { StudentService } from "../services/student.service";
 
 export const UpdateStudentExam = async (req: clientRequest, res: Response) => {
   const { _id } = req.user;
-  const {exams} = req.body;
-  
-  const studentService = new StudentService()
+  const { exams } = req.body;
 
-  const response = await studentService.updateStudentExamsById(_id,exams)
+  const studentService = new StudentService();
+
+  const response = await studentService.updateStudentExamsById(_id, exams);
   if (response["status"] == 200) {
-    res.status(200).json({status:response["status"] , message: response["message"] });
+    res
+      .status(200)
+      .json({ status: response["status"], message: response["message"] });
   } else {
-    res.status(response["status"]).json({status:response["status"] , message: response["message"] });
+    res
+      .status(response["status"])
+      .json({ status: response["status"], message: response["message"] });
   }
-  
-
-
 };
