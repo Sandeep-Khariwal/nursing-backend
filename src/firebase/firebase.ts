b56eaba4e6ffeb6ae8b8458783e0d88415bf2e8c
sandeep khariwal <sandeepkharival123@gmail.com>
2025-09-03 12:01:17 +0530

    pkgjson and aws

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,9 +2,6 @@
 import admin from 'firebase-admin';
 
 const serviceAccount = require("./serviceAccountKey.json")
-console.log("serviceAccount : ",serviceAccount);
-
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
