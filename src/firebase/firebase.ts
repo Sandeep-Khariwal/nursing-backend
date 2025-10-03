185caaf08c5d70554c927c26b1003bbaa71eef6c
sandeep khariwal <sandeepkharival123@gmail.com>
2025-10-03 11:41:55 +0530

    question image done 0.1

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,10 +1,6 @@
 import admin from "firebase-admin";
 
-const serviceAccount: admin.ServiceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-};
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG!);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
